1b6eb3f5dc9beb49712593a43b0111067be2fe25
Uday-132 <udayvenkatanagasai@gmail.com>
2026-02-07 12:46:57 +0530

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
+let isConnected = false;
 
 const connectDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        isConnected = true;
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        // Do not exit process in serverless environment
+        // process.exit(1); 
     }
 };
 
